d31d4067a1fd147c97ac2f5fcf44af7e0f1c5fa5
DevAmbani <147029224+DevAmbani@users.noreply.github.com>
2026-06-29 07:59:51 -0500

    Time: 46 ms (39.36%), Space: 20.3 MB (62.62%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        dist = defaultdict(int)
+        
+        for i in range(n):
+            dist[i] = float('inf')
+        
+        dist[src] = 0
+
+        for i in range(k+1):
+
+            temp = dist.copy()
+            for source, dest, cost in flights:
+                new_dist = dist[source] + cost
+                if new_dist < temp[dest]:
+                    temp[dest] = new_dist
+            dist = temp
+        
+        if dist[dst] == float('inf'):
+            return -1
+        
+        return dist[dst]
